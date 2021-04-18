65064a5195e030ec7d96cd7ab6a549273a092400
ValentinVignal <valentin.vignal.dev@outlook.fr>
2021-04-19 00:19:24 +0800

    :loud_sound: Log matched files

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import * as glob from 'glob';
+import * as fs from 'fs';
 import { actionOptions } from './utils/ActionOptions';
 
 async function main() {
   const files = glob.sync(actionOptions.matcher);
   console.log('files\n', files);
+
+  console.log(fs.readdirSync('./'));
 }
 
 main();
